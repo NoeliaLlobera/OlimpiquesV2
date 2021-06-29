514a67f1b499da04df3ac52ef5d5400e32a9d295
Noelia Llobera <noelialloberas@gmail.com>
2021-06-30 00:06:16 +0200

    AOS fix

--- codi.js
+++ codi.js
@@ -1,4 +1,4 @@
-// JavaScript Document
+// CREDITS
 				var modal = document.getElementById("modalcredits");
 				var btn = document.getElementById("botocredits");
 				var span = document.getElementsByClassName("creu")[0];
@@ -16,32 +16,3 @@
 				}
 
 
-
-
-// PRELOADER
-
-// window.onload = function(){
-// 	alert("Pàgina correctament carregada");
-// 	document.querySelector(".preloader").style.display = "none";
-	
-// }
-
-
-// window.onload = function carrega (){	
-// 	alert("Pàgina correctament carregada");	
-// 	var x = document.getElementById("preloader2");
-// 	x.style.display = "none";
-
-// 	var y = document.getElementById("amaga");
-// 	y.style.display = "block";
-// }
-
-
-// window.onload = function carrega (){	
-// 	alert("carregat");	
-// 	var x = document.getElementById("preloader");
-// 	x.style.display = "none";
-
-// 	var y = document.getElementById("amaga");
-// 	y.style.display = "block";
-// }
